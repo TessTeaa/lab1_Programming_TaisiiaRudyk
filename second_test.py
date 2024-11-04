1e83aede678618fafd759ffc22e560672b6dd4af
TessTeaa <taisiiarudyk@gmail.com>
2024-11-04 19:20:16 +0200

    Create second_test.py

---
+++ second_test.py
@@ -0,0 +1,8 @@
+from laba2 import process_adjectives 
+def test_word_filter():
+    result = process_adjectives("крута великий миле веселі гарна", "comparative")
+    assert result == " ".join(["крутіша", "більший", "миліше", "веселіші", "краща"])
+ 
+def test_word_filter():
+    result = process_adjectives("крута великий миле веселі гарна", "superlative")
+    assert result == " ".join(["найкрутіша", "найбільший","наймиліше", "найвеселіші", "найкраща"])
